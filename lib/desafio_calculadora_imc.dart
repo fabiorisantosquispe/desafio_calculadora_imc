67612d5df85f525f80290cc2bba209ef1d6ba7d9
Fábio Ribeiro dos Santos Quispe <23216054@aluno.univesp.br>
2023-09-17 11:52:09 -0300

    Tratar as excessões

--- lib/desafio_calculadora_imc.dart
+++ lib/desafio_calculadora_imc.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'exceptions/custom_exceptions.dart';
+
 class Pessoa {
   String nome;
   double peso;
@@ -9,15 +11,36 @@ class Pessoa {
 }
 
 void execute() {
-  print("Digite o nome da pessoa:");
-  var nome = stdin.readLineSync();
-  print(nome);
+  try {
+    print("Digite o nome da pessoa:");
+    var nome = stdin.readLineSync();
+    if (nome == null || nome.isEmpty) {
+      throw CampoEmBrancoException();
+    }
+    print(nome);
 
-  print("Digite o peso (em kg):");
-  var peso = stdin.readLineSync();
-  print(peso);
+    print("Digite o peso (em kg):");
+    var pesoInput = stdin.readLineSync();
+    var peso = double.tryParse(pesoInput ?? "");
+    if (peso == null || peso <= 0) {
+      throw ValorInvalidoException();
+    }
+    print(peso);
 
-  print("Digite a altura (em metros):");
-  var altura = stdin.readLineSync();
-  print(altura);
+    print("Digite a altura (em metros):");
+    var alturaInput = stdin.readLineSync();
+    var altura = double.tryParse(alturaInput ?? "");
+    if (altura == null || altura <= 0) {
+      throw ValorInvalidoException();
+    }
+    print(altura);
+  } catch (e) {
+    if (e is CampoEmBrancoException) {
+      print("Erro: ${e.mensagem()}");
+    } else if (e is ValorInvalidoException) {
+      print("Erro: ${e.mensagem()}");
+    } else {
+      print("Erro desconhecido: $e");
+    }
+  }
 }
