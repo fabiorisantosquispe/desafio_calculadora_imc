793530d942dbe36366946feeb978d291e38850c3
Fábio Ribeiro dos Santos Quispe <23216054@aluno.univesp.br>
2023-09-17 11:34:07 -0300

    Ler dados do terminal

--- lib/desafio_calculadora_imc.dart
+++ lib/desafio_calculadora_imc.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 class Pessoa {
   String nome;
   double peso;
@@ -5,4 +7,17 @@ class Pessoa {
 
   Pessoa(this.nome, this.peso, this.altura);
 }
-void execute() {}
+
+void execute() {
+  print("Digite o nome da pessoa:");
+  var nome = stdin.readLineSync();
+  print(nome);
+
+  print("Digite o peso (em kg):");
+  var peso = stdin.readLineSync();
+  print(peso);
+
+  print("Digite a altura (em metros):");
+  var altura = stdin.readLineSync();
+  print(altura);
+}
